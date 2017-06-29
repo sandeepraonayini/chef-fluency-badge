0351831f01c376ccee79d8aafd3ca9820a2ce562
root <root@dexperts4.mylabserver.com>
2017-06-29 16:46:20 +0000

    adding new files to remote

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -4,12 +4,20 @@
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 #installing apache and starting the service 
+
+if node['platform_family'] == "rhel"
+        package = "httpd"
+elseif node['platform_family'] == "debain"
+        package = "apache2"
+end
+
 package 'apache2' do
-   package_name 'httpd'
+   package_name package
        action :install
 end
 
 service 'apache2' do
   service_name 'httpd'
-   action[:start, :enable]
+   action [:start, :enable]
 end
+
